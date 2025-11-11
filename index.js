5fa1f44d66f63bdc004694832771eb543dc197a7
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-11 16:07:36 +0700

    Implementasi endpoint POST /create untuk membuat & menyimpan API key

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const path = require('path')
-const mysql = require('mysql2/promise') // <-- BARU
+const crypto = require('crypto') // <-- BARU
+const mysql = require('mysql2/promise')
 const app = express()
 const port = 3000
 
-// Pool Koneksi Database (BARU)
+// Pool Koneksi Database
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: 'Simbada662004', // Password Anda
+    password: 'Simbada662004',
     database: 'apikey',
-    port: 3309,              // Port Anda
+    port: 3309,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
@@ -22,6 +23,22 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Endpoint GET '/'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+// Endpoint POST '/create' (BARU)
+app.post('/create', async (req, res) => {
+    try {
+        const randomBytes = crypto.randomBytes(16).toString('hex').toUpperCase()
+        const apiKey = `Daffa-${randomBytes.slice(0, 8)}-${randomBytes.slice(8, 16)}-${randomBytes.slice(16, 24)}-${randomBytes.slice(24, 32)}`
+
+        const sql = "INSERT INTO apikey (api_key_value) VALUES (?)"
+        await pool.query(sql, [apiKey])
+        
+        res.json({ apiKey })
+    } catch (err) {
+        console.error("Gagal menyimpan ke DB:", err)
+        res.status(500).json({ error: "Gagal memproses key." })
+    }
 })
 
 // Jalankan server
