3bc7fd861c6c4dad90110f19366001c7dff6c1c2
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-11 16:06:05 +0700

    Tambahkan rute untuk menyajikan file statis dan index.html

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
+const path = require('path') // <-- BARU
 const app = express()
 const port = 3000
+
+// Middleware (BARU)
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Endpoint GET '/' (BARU)
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 // Jalankan server
 app.listen(port, () => {
