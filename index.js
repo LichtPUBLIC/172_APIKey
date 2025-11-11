bd0234061dd5ef371dec0988fa69feeb5a56be94
LichtPUBLIC <daffaalyandra65@gmail.com>
2025-11-11 16:07:08 +0700

    Siapkan koneksi pool database MySQL

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
-const path = require('path') // <-- BARU
+const path = require('path')
+const mysql = require('mysql2/promise') // <-- BARU
 const app = express()
 const port = 3000
 
-// Middleware (BARU)
+// Pool Koneksi Database (BARU)
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'Simbada662004', // Password Anda
+    database: 'apikey',
+    port: 3309,              // Port Anda
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')))
 
-// Endpoint GET '/' (BARU)
+// Endpoint GET '/'
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
